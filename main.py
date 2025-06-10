98f3548cad326728b899620730dc0984d07afedc
DiegoCarcamo <diegofdiaz1717@gmailc.com>
2025-06-10 10:24:10 -0600

    vecto db ready

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from langchain_ollama import OllamaEmbeddings
 import os
 import redis
 
-
+from queries import retriever
 from langchain_redis import RedisConfig, RedisVectorStore
 
 
@@ -33,11 +33,11 @@ def create_vector_store():
 
 
     embeddings = OllamaEmbeddings(
-        model="nomic-embed-text",
+        model="mxbai-embed-large",
         base_url="http://172.16.226.32:11434"
     )
     config = RedisConfig(
-        index_name="newsgroups",
+        index_name="gnuhealth",
         redis_url=REDIS_URL,
         metadata_schema=[
             {"name": "category", "type": "tag"},
@@ -57,13 +57,59 @@ def load_docs():
             kbs[doc]=content
     return kbs
 
-def main():
+
+
+def store_procedure():
     if test_redis():
-        pass
         vec_store=create_vector_store()
 
+    metadata=[{"category":"party"},
+              {"category":"pathology"},
+              {"category":"patient"},
+              {"category":"pregnancy"}]
+    
     kbs=load_docs()
-    print(kbs["party_party_table_gpt.txt"])
+    ids=vec_store.add_texts(kbs.values(),metadata)
+    print(ids[0:2])
+
+
+
+def direct_query(text_input):
+    query = "How to read the pregnancy table"
+    vec=create_vector_store()
+    results = vec.similarity_search(query, k=2)
+    
+    print("how to read pathology table")
+    for doc in results:
+        print(f"Content: {doc.page_content[:100]}...")
+        print(f"Metadata: {doc.metadata}")
+        print()
+    
+def main():
+    vector_store=create_vector_store()
+    input_text="I need to list all the births  for this month "
+    direct_query(input_text)
+    ret=retriever(input_text,vector_store)
+    context=""
+    for doc in ret:
+        context+=str(doc)+"\n"
+
+        
+
+
+    # 'w' mode means "write" mode. It will create the file if it doesn't exist,
+    # or overwrite its content if it does.
+    with open("out.txr", 'w', encoding='utf-8') as file:
+        file.write(context)
+        print(f"Successfully wrote to out.txt")
+
+    print(ret)
+
+
+
+    
+
+
 
 
 main()
